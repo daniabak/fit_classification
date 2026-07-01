d339fec12257d7e8291a9b7a1eb9e1add2d59af7
FITE Team <student@example.com>
2026-07-01 22:10:21 +0300

    Track new submission with DVC

--- scripts/run_mlflow_ui.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d %~dp0\..
-mlflow ui --backend-store-uri sqlite:///mlflow.db
--- scripts/run_mlflow_ui.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-cd "$(dirname "$0")/.."
-mlflow ui --backend-store-uri sqlite:///mlflow.db
